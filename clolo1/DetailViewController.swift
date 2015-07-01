ed221954d0a1142fe901015d6bcdccc4e8613846
mtrk3 <m.3.pguitar@gmail.com>
2015-07-02 01:12:35 +0900

    add photo

--- clolo1/DetailViewController.swift
+++ clolo1/DetailViewController.swift
@@ -4,20 +4,25 @@
 //
 //  Created by 奥野美波 on 2015/06/30.
 //  Copyright (c) 2015年 奥野美波. All rights reserved.
-//
+
 
 import UIKit
 
-class DetailViewController: UIViewController {
-
+class DetailViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @IBOutlet weak var MainImageView: UIImageView?
+    @IBAction func addButton(sender: AnyObject) {
+        self.pickImageFromCamera()
+        self.pickImageFromLibrary()
+    }
+    
+//    
+//    let callnumberArray: NSArray = ["tel://0334149089","tel://0357909998","tel://0357909998","tel://0337954177","tel://0368040660","tel://0000000000","tel://0000000000","tel://0334108563"]
+//
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
-    }
-
-    @IBAction func call(){
-        UIApplication.sharedApplication().openURL(NSURL(string: "tel://0000000000")!)
     }
     
     override func didReceiveMemoryWarning() {
@@ -25,15 +30,50 @@ class DetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // 写真を撮ってそれを選択
+    func pickImageFromCamera() {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
+            let controller = UIImagePickerController()
+            controller.delegate = self
+            controller.sourceType = UIImagePickerControllerSourceType.Camera
+            self.presentViewController(controller, animated: true, completion: nil)
+        }
+    }
+    
+    // ライブラリから写真を選択する
+    func pickImageFromLibrary() {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) {
+            let controller = UIImagePickerController()
+            controller.delegate = self
+            controller.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+            self.presentViewController(controller, animated: true, completion: nil)
+        }
+    }
+    
+    
+    // 写真を選択した時に呼ばれる
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        if info[UIImagePickerControllerOriginalImage] != nil {
+            let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+            MainImageView?.image = image
+            //画像のアスペクト比を維持しUIImageViewサイズに収まるように表示
+            //MainImageView?.contentMode = UIViewContentMode.ScaleAspectFit
+            println(image)
+        }
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    
+    }
 
+
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
--- clolo1/ViewController.swift
+++ clolo1/ViewController.swift
@@ -10,17 +10,17 @@ import UIKit
 
  var selectedLabel: UILabel!
 
-
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    
-     @IBOutlet var tableView: UITableView?
-  
     let imgArray: NSArray = ["DoStyle2.jpg","LANP.jpg","VALON.jpg","ヒッチハイカー.jpg","Potagerlone.jpg","RAGLAMAGLA2.jpg","RAGLAMAGLA1.jpg","SPOOKY?.jpg","RINKAN.jpg","Recycle&SelectShopArmadio.jpg","スティックアウト.jpg","GRAPEFRUITMOON.jpg","SANKA.jpg","FLAMINGO.jpg","SUNNYSIDEUP.jpg","３びきの子ねこ.jpg","SMOG.jpg","RubyTuesday.jpg","UTA.jpg","TRADE★MARK.jpg","saLad bowL.jpg","hickory.jpg","MAMMOTHJUMP.jpg","BIGRUBY.jpg","HOOCHIECOOCHIE.jpg","GUADALUPE.jpg","ヴォーテしもきた.jpg","soma.jpg","Like.jpg","Pmax.jpg","Bulsaras.jpg","ばら商店.jpg","thalassicus.jpg"]
 
     let label1Array: NSArray = ["DoStyle","Lanp","VALON","ヒッチハイカ-","Potager lone","RAGLA MAGLA 下北沢2号店","RAGLA MAGLA 下北沢1号店","SPOOKY?","RINKAN 下北沢店","Recycle & Select Shop Armadio","スティックアウト","GRAPEFRUIT MOON 下北沢","SANKA","FLAMINGO 下北沢店","SUNNY SIDE UP 下北沢店","３びきの子ねこ","SMOG","Ruby Tuesday","UTA","TRADE★MARK","saLad bowL","hickory","MAMMOTH JUMP","BIG RUBY","HOOCHIE COOCHIE 北口店","GUADALUPE","ヴォーテしもきた","soma","Like","P.max","Bulsaras","リサイクルショップ ばら商店 下北沢店","thalassicus"]
     
   
     let label2Array: NSArray = ["東京都世田谷区北沢2-19-16","東京都世田谷区北沢2-25-8　東洋百貨店 №７","東京都世田谷区北沢2-25-8 東洋百貨店内No23","東京都世田谷区北沢2-23-6 スミレビル3","東京都世田谷区代沢5-33-3ボート代沢","東京都世田谷区北沢2-30-13 ファインコート２Ｆ","東京都世田谷区北沢2-30-10 浜辺ビル201","東京都世田谷区代沢5-36-17 膳場ビル２F","東京都世田谷区北沢2-33-5新力ビル1F","東京都世田谷区北沢1-32-17","東京都世田谷区北沢2-14-16-2F","東京都世田谷区北沢2-2-11 Comodo I 1F","東京都世田谷区北沢2-40-17","東京都世田谷区北沢2-25-12 リサビル1F","東京都世田谷区北沢２－２５－１３　２Ｆ","東京都世田谷区北沢2-25-8 東洋百貨店内","東京都世田谷区北沢2-25-8 東洋百貨店","東京都世田谷区代沢5-36-16　井上ビル1C","東京都世田谷区北沢2-9-25 下北沢ビル2F","東京都世田谷区北沢2-23-12 アドレス北沢1F","東京都世田谷区代沢 5-36-16","東京都世田谷区代沢5-29-17飯田ハイツ１Ｆ","東京都世田谷区北沢2-26-10 2F","東京都世田谷区北沢2-24-6 北口ビル1F","東京都世田谷区北沢2-30-2","東京都世田谷区北沢2-26-23　オーブ井部ビル1F","東京都世田谷区代田6-7-24 ルミナス24","東京都世田谷区北沢2-12-2 ミナナミコーポ102","東京都世田谷区北沢2-25-8 東洋百貨店内","東京都世田谷区代沢5-32-7","東京都世田谷区代沢5-28-14","東京都世田谷区代沢5-8-10","東京都世田谷区北沢2-30-3"]
+
+
+ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+     @IBOutlet var tableView: UITableView?
 
     
     //tableViewの紐付け
